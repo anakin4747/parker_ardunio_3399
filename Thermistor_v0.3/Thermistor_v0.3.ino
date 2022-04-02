93ed2ebe86b8c47ff5214d2aba962c5c0d842fe3
Anakin Childerhose <anakin@childerhose.ca>
2022-04-02 17:34:41 -0400

    secondtest

--- Thermistor_v0.3/Thermistor_v0.3.ino
+++ Thermistor_v0.3/Thermistor_v0.3.ino
@@ -1,7 +1,10 @@
 #include <math.h>
-int analogPin = A0; // potentiometer wiper (middle terminal) connected to analog pin 3
-                    // outside leads to ground and +5V
-int val = 0;  // variable to store the value read
+int temp1Pin = A0; //Temp1 node              
+int temp2Pin = A1; 
+
+// outside leads to ground and +5V
+
+int Vout = 0;  // variable to store the value read
 double temp;
 double volt;
 double resist;
@@ -11,14 +14,18 @@ void setup() {
 }
 
 void loop() {
-  float val = analogRead(analogPin);  // read the input pin
-  volt = val *  5.0 / 1024.0 ;
-  resist = ((5.00 / volt) - 1.00) ;
-  temp = 3455.00 / (11.588 + log(resist));
-  temp = temp - 273.15;
+  float v1 = analogRead(temp1Pin);  // read the input pin    
+  float v2 = analogRead(temp2Pin);  
+  v1 = v1 *  5.0 / 1024.0 ;
+  v2 = v2 *  5.0 / 1024.0 ;
+  Vout = v1 - v2;
 
-  Serial.print("The temperature is ");
-  Serial.print(temp);    
-  Serial.println(" degrees Celcius.");
-  delay(4000);
+  resist = (10000/(Vout/5.0000 + 0.5)) - 10000;
+
+  Serial.print("\nVout is ");
+  Serial.print(v1); 
+  delay(2000);
+
+
+  
 }
