4c0209510ec49929ecc169a0fb6511a60ab0fe30
Anakin Childerhose <anakin@childerhose.ca>
2022-04-03 12:12:00 -0400

    accurate thermistor code with 2 pins

--- Thermistor_v0.3/Thermistor_v0.3.ino
+++ Thermistor_v0.3/Thermistor_v0.3.ino
@@ -4,9 +4,8 @@ int temp2Pin = A1;
 
 // outside leads to ground and +5V
 
-int Vout = 0;  // variable to store the value read
+float Vout = 0;  // variable to store the value read
 double temp;
-double volt;
 double resist;
 
 void setup() {
@@ -16,14 +15,29 @@ void setup() {
 void loop() {
   float v1 = analogRead(temp1Pin);  // read the input pin    
   float v2 = analogRead(temp2Pin);  
-  v1 = v1 *  5.0 / 1024.0 ;
-  v2 = v2 *  5.0 / 1024.0 ;
+  v1 = v1 *  5.0 / 1023.0 ;
+  v2 = v2 *  5.0 / 1023.0 ;
   Vout = v1 - v2;
 
-  resist = (10000/(Vout/5.0000 + 0.5)) - 10000;
+  resist = (10000/((Vout/5.0000) + 0.5)) - 10000;
 
+  
+  //temp = 3455.00 / (11.588 + log(resist));
+  temp = 1 / ((log(resist/10000.000)/3428) + (1/298.15));
+  
+  temp = temp - 273.15;
+  
+
+  Serial.print("\nV1 is ");
+  Serial.print(v1);
+  Serial.print("\nV2 is ");
+  Serial.print(v2);
   Serial.print("\nVout is ");
-  Serial.print(v1); 
+  Serial.print(Vout);
+  Serial.print("\nResistance is ");
+  Serial.print(resist);
+  Serial.print("\nTemp is ");
+  Serial.print(temp);
   delay(2000);
 
 
